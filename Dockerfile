f56b6270c39a201ad7b22b2662f28f4191ab2d51
Pioneer18 <sellsj14@gmail.com>
2024-12-19 22:07:46 -0500

    1. no longer using Jest, the requirement to use Babel so jest can work with typescript was breaking Next.js build. Switched to vitest 2. set up vitest.config.mts 3. reformatted the tsconfig file slightly, same content 4. using --omit=dev for the npm install in the docker file 5. updated chatComponent.test.tsx to use vitest instead of jest
    testsuite is passing, and npm run build works! should be a useable testing setup

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:18 as build
 
 WORKDIR /app
 COPY package*.json ./
-RUN npm install
+RUN npm install --omit=dev
 
 # Copy the rest of the application files
 COPY . .
---
+++ src/app/ChatComponent/chatComponent.test.tsx
@@ -0,0 +1,28 @@
+import {expect, test} from 'vitest'
+import { render, screen } from '@testing-library/react';
+import ChatComponent from './chatComponent';
+
+
+test('displays div for messages', () => {
+    render(<ChatComponent />);
+    
+    // Simply testing the components are rendering for now
+    const messagesList = screen.getByRole('list', { name: 'messages' });
+    expect(messagesList).toBeDefined();
+});
+
+test('displays input field for requests', () => {
+    render(<ChatComponent />);
+    
+    // Simply testing the components are rendering for now
+    const aiRequest = screen.getAllByLabelText('ai request input');
+    expect(aiRequest).toBeDefined();
+});
+
+test('displays button to submit requests', () => {
+    render(<ChatComponent />);
+    
+    // Simply testing the components are rendering for now
+    const aiRequest = screen.getAllByLabelText('submit ai request');
+    expect(aiRequest).toBeDefined();
+});
